91c124bc289fd2384cefe8ba72c6e8a141c39b2c
Spencer K Chuong <spencerchuong0@gmail.com>
2021-09-01 22:33:46 -0700

    Made entering locations more efficient

--- Food_Decider_Program.py
+++ Food_Decider_Program.py
@@ -3,7 +3,9 @@ import random
 
 #Create list of restaurants for each area in SoCal
 def preexisting_list():
-  #Ask which location does the user want
+  print()
+  print("Locations: \nFountain Valley (FV) \nLos Angeles (LA) \nWestminster \nGarden Grove (GG) \nSanta Ana \nIrvine \nTorrance \nHuntington Beach (HB)")
+  print()
   location_picker = input("Which location do you want a list of?: ")
   
   #Create an if statement if the user wants a list from Fountain Valley
@@ -26,7 +28,7 @@ def preexisting_list():
   elif location_picker == "Irvine":
     return Irvine()
 
-  #Create an if statement if the user wants a list from Tirrance 
+  #Create an if statement if the user wants a list from Torrance 
   elif location_picker == "Torrance":
     return Torrance()
 
@@ -51,8 +53,8 @@ def Torrance():
 
 def Huntington_Beach():
   #Create list for Hubntington Beach
-  HB_List = ["Sushi On Fire", "Duke's Huntington Beach", "Charlie's Gyros", 
-  "Blue Gold", "Nori Poke Sushi"]
+  HB_List = ["Sushi On Fire", "Duke's Huntington Beach", 
+  "Charlie's Gyros", "Blue Gold", "Nori Poke Sushi"]
   return HB_List
   
 def Irvine():
@@ -81,8 +83,9 @@ def Westminster():
 
 def FV():
   #Create list for Fountain Valley
-  Fv_list = ["Ikram Grill", "Hot-Off-the-Gril", "Vox Kitchen", "Kensho", "Nep Cafe", "Red Flame", 
-  "Sabrosada", "Taco Bell", "Project Poke", "Full Moon", "Nikado", "Dennys"]
+  Fv_list = ["Ikram Grill", "Hot-Off-the-Gril", "Vox Kitchen", "Kensho", 
+  "Nep Cafe", "Red Flame", "Sabrosada", "Taco Bell", "Project Poke", 
+  "Full Moon", "Nikado", "Dennys"]
   return Fv_list
 
 def LA():
@@ -103,28 +106,25 @@ def main():
   Food_list = []
   #Create a variable
   i = 0
-  print("Hello! I will help you decide places to go since you can not decide! (Orange and Los Angeles County Edition")
+  print("Hello! I will help you decide places to go since you can not decide!")
   print()
-  option1 = input("Do you want a preexisting list of foods or do you want to create a list? Enter Preexisting or New List: ")
+  option1 = input("Do you want a preexisting list of foods or do you want to create a list? \nEnter 'Preexisting'(provides SoCal location lists) or 'New List': ")
   
   #Create an if statement if the user wants a preexisting list
   if option1 == 'Preexisting':
     Food_list = preexisting_list()
-    
   else:
-    #Ask the user to input how many places they can't choose 
-    num_inputs = int(input("Please enter the number of places you can not decide on: "))
     #Create a loop that will loop as many times as what number was inputted
-
-    while i < num_inputs:
+    x = True
+    while x == True:
       #Ask the user to input the name of the place
-      food_name = input("Enter the name of the place: ")
-      #Add it to the list
-      Food_list.append(food_name)
-      i+=1
-    print()
-    #Display the list of places entered
-
+      food_name = input("Enter the name of the place. Enter 'End' to stop: ")
+      if food_name == "End":
+        x = False
+      else:
+        #Add it to the list
+        Food_list.append(food_name)
+  #Display the list of places entered
   print("List of places:", Food_list)
   print()
   #Get specific by asking if the user is craving any type of food
@@ -134,24 +134,20 @@ def main():
   #Create a variable
   k = 0
   #Create an if statement if the user is craving something
-
   if question2 == 'Yes':
-    #Ask the user what thery crave
-    num_of_food_craves = int(input("How many places from what you inputted sells what you crave?: "))
-    print()
-
     #Create a while loop that will loop as many times as the number inputted
-    while k < num_of_food_craves:
-       #Have the user input what they are craving
-       food_finder = input("What is the name(s) of the place(s)?: ")
+    y = True
+    while y == True:
+      #Have the user input what they are craving
+      food_finder = input("What is the name(s) of the place(s)? Enter 'End' to stop adding: ")
+      if food_finder == "End":
+        y = False
+      else:
        #Add the name to the list
        specific_list.append(food_finder)
-       #Add one to the variable
-       k+=1
-     #Add the list as an arguement to the call of the function
+      #Add the list as an arguement to the call of the function
     food_decider1(specific_list)
-   #Create an elif statement if the user is craving anything
-
+  #Create an elif statement if the user is craving anything
   elif question2 == "No":
     food_decider1(Food_list)
     #Create an else statement is the list has nothing of what the user craves
